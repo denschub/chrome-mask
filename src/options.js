c3f3244340504be912627360fde32a24b759ab28
Dennis Schubert <dschubert@mozilla.com>
2025-03-09 22:20:28 +0100

    Add UI to manually add a site

--- src/options.js
+++ src/options.js
@@ -1,9 +1,52 @@
 const enabledHostnames = new EnabledHostnamesList();
 
-async function updateUiState() {
-  document.getElementById("masked-sites-title").innerText = browser.i18n.getMessage("maskedSitesTitle");
+async function initUi() {
+  [
+    ["add-site-hostname-explanation", "addSiteHostnameExplanation"],
+    ["add-site-title", "addSiteTitle"],
+    ["masked-sites-title", "maskedSitesTitle"],
+  ].forEach(([id, i18nKey]) => {
+    document.getElementById(id).innerText = browser.i18n.getMessage(i18nKey);
+  });
 
+  document.getElementById("add-site-button").value = browser.i18n.getMessage("addSiteButton");
+
+  setupAddForm();
   setupSiteList();
+}
+
+function tryValidateHostname(input) {
+  if (URL.canParse(input)) {
+    return URL.parse(input).hostname;
+  }
+
+  if (URL.canParse(`https://${input}`)) {
+    return URL.parse(`https://${input}`).hostname;
+  }
+
+  return undefined;
+}
+
+function setupAddForm() {
+  const inputEl = document.getElementById("add-site-input");
+  document.getElementById("add-site-form").addEventListener("submit", async (ev) => {
+    ev.preventDefault();
+
+    const maybeHostname = tryValidateHostname(inputEl.value);
+    if (!maybeHostname) {
+      alert(browser.i18n.getMessage("addSiteErrorInvalid"));
+      return false;
+    }
+
+    if (enabledHostnames.contains(maybeHostname)) {
+      alert(browser.i18n.getMessage("addSiteErrorAlreadyActive"));
+      return false;
+    }
+
+    await enabledHostnames.add(maybeHostname);
+    inputEl.value = "";
+    window.location.reload();
+  });
 }
 
 function setupSiteList() {
@@ -30,7 +73,7 @@ function setupSiteList() {
       deleteButton.textContent = browser.i18n.getMessage("siteListRemoveButton");
       deleteButton.addEventListener("click", async () => {
         await enabledHostnames.remove(hostname);
-        await updateUiState();
+        window.location.reload();
       });
 
       siteListItem.append(hostnameLabel, deleteButton);
@@ -40,13 +83,12 @@ function setupSiteList() {
 
 document.addEventListener("DOMContentLoaded", async () => {
   await enabledHostnames.load();
-  await updateUiState();
+  await initUi();
 
   browser.runtime.onMessage.addListener(async (msg) => {
     switch (msg.action) {
       case "enabled_hostnames_changed":
-        await enabledHostnames.load();
-        await updateUiState();
+        window.location.reload();
         break;
       default:
         throw new Error("unexpected message received", msg);
