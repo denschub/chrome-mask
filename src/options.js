3cfb990f9168a9d9e90063266b246a755f429a74
Dennis Schubert <dschubert@mozilla.com>
2025-03-09 19:11:02 +0100

    Options UI: Refactorings, add a "remove" button.

--- src/options.js
+++ src/options.js
@@ -1,24 +1,41 @@
 const enabledHostnames = new EnabledHostnamesList();
 
 async function updateUiState() {
-  const siteList = document.getElementById("site-list");
-  const siteListTitle = document.getElementById("site-list-title");
-  const siteListDescription = document.getElementById("site-list-description");
+  document.getElementById("masked-sites-title").innerText = browser.i18n.getMessage("maskedSitesTitle");
 
-  siteListTitle.innerText = browser.i18n.getMessage("siteListTitle");
-  siteListDescription.innerText = browser.i18n.getMessage("siteListDescription");
+  setupSiteList();
+}
 
-  if (enabledHostnames.size() > 0) {
-    enabledHostnames.get_values().forEach((hostname) => {
-      const siteListItem = document.createElement("li");
-      siteListItem.textContent = hostname;
-      siteList.appendChild(siteListItem);
-    });
-  } else {
+function setupSiteList() {
+  const siteList = document.getElementById("masked-sites");
+  siteList.innerHTML = "";
+
+  if (enabledHostnames.size() < 1) {
     const siteListItem = document.createElement("p");
     siteListItem.innerText = browser.i18n.getMessage("siteListEmpty");
     siteList.appendChild(siteListItem);
+    return;
   }
+
+  [...enabledHostnames.get_values()]
+    .sort((a, b) => a.localeCompare(b))
+    .forEach((hostname) => {
+      const siteListItem = document.createElement("div");
+      siteListItem.classList.add("site-list-item");
+
+      const hostnameLabel = document.createElement("p");
+      hostnameLabel.textContent = hostname;
+
+      const deleteButton = document.createElement("button");
+      deleteButton.textContent = browser.i18n.getMessage("siteListRemoveButton");
+      deleteButton.addEventListener("click", async () => {
+        await enabledHostnames.remove(hostname);
+        await updateUiState();
+      });
+
+      siteListItem.append(hostnameLabel, deleteButton);
+      siteList.appendChild(siteListItem);
+    });
 }
 
 document.addEventListener("DOMContentLoaded", async () => {
@@ -28,8 +45,8 @@ document.addEventListener("DOMContentLoaded", async () => {
   browser.runtime.onMessage.addListener(async (msg) => {
     switch (msg.action) {
       case "enabled_hostnames_changed":
+        await enabledHostnames.load();
         await updateUiState();
-        window.location.reload();
         break;
       default:
         throw new Error("unexpected message received", msg);
