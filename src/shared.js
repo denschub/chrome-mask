bfe380c00d314676fbaa6c6f37679fda131aea20
Dennis Schubert <mail@dennis-schubert.de>
2025-11-12 20:09:57 +0100

    Update default/fallback Chrome version number

--- src/shared.js
+++ src/shared.js
@@ -71,7 +71,7 @@ class ChromeUAStringManager {
 
   // This are just fallbacks in the case we somehow have to make a request before everything is loaded.
   #currentPlatform = "win";
-  #currentUAString = `Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36`;
+  #currentUAString = `Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36`;
 
   async init() {
     const platformInfo = await browser.runtime.getPlatformInfo();
@@ -93,7 +93,7 @@ class ChromeUAStringManager {
   }
 
   async buildUAStringFromStorage() {
-    let currentChromeVersion = "134";
+    let currentChromeVersion = "142";
 
     const storedMajorVersion = (await browser.storage.local.get("remoteStorageVersionNumber"))
       ?.remoteStorageVersionNumber?.version;
